c59928fb30182e5dcdeed9a109f5abdd64dbfd89
James Reeves <jreeves@weavejester.com>
2013-03-15 11:48:14 +0000

    Updated clj-time dependency

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [clj-time "0.3.8"]])
+                 [clj-time "0.4.5"]])
